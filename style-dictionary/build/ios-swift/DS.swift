d9dfa5d6e085ca0a4c4fce367029d59087e8bf9d
Dmitry-anna-money <Dmitry-anna-money@users.noreply.github.com>
2022-01-28 14:13:53 +0000

    Tokens updated

---
+++ style-dictionary/build/ios-swift/DS.swift
@@ -0,0 +1,26 @@
+
+//
+// DS.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 28 Jan 2022 14:13:52 GMT
+
+
+import UIKit
+
+public class DS {
+    public static let globalBlue = #2f80ed
+    public static let globalGreen = #219653
+    public static let globalHorizontal8 = 8
+    public static let globalLarge = 8
+    public static let globalLargeCopy = 8
+    public static let globalMedium = 6
+    public static let globalMediumBorderWidth = 2
+    public static let globalRed = #ff0000
+    public static let globalSmall = 4px
+    public static let globalSmallBorderWidth = 1
+    public static let globalTypographySubtitle = [object Object]
+    public static let globalTypographyTitle = [object Object]
+    public static let globalVertical8 = 8
+}

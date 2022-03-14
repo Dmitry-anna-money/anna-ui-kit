da6af3aa15e1538f7a37566c1084ced2568d58bb
daocacao <mrdaocacao@gmail.com>
2022-03-14 18:48:37 +0400

    update components

--- android/tiger-ui-kit/src/main/java/anna/money/tiger_ui_kit/text_message/AnnaTextMessage.kt
+++ android/tiger-ui-kit/src/main/java/anna/money/tiger_ui_kit/text_message/AnnaTextMessage.kt
@@ -5,7 +5,10 @@ import android.util.AttributeSet
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
@@ -29,44 +32,95 @@ fun AnnaTextMessage(
     position: MessagePosition,
     onClick: () -> Unit = {},
 ) {
-    Text(
-        modifier = Modifier
-            .clip(
-                shape = when (position) {
-                    MessagePosition.Start -> RoundedCornerShape(
-                        topStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_start_borderRadius_top_left),
-                        topEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_start_borderRadius_top_right),
-                        bottomStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_start_borderRadius_bottom_left),
-                        bottomEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_start_borderRadius_bottom_right),
-                    )
-                    MessagePosition.Middle -> RoundedCornerShape(
-                        topStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_mid_borderRadius_top_left),
-                        topEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_mid_borderRadius_top_right),
-                        bottomStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_mid_borderRadius_bottom_left),
-                        bottomEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_mid_borderRadius_bottom_right),
-                    )
-                    MessagePosition.End -> RoundedCornerShape(
-                        topStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_end_borderRadius_top_left),
-                        topEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_end_borderRadius_top_right),
-                        bottomStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_end_borderRadius_bottom_left),
-                        bottomEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_end_borderRadius_bottom_right),
-                    )
-                },
-            )
-            .background(
-                color = colorResource(id = R.color.tigerds_bubble_chat_anna_background_color),
-            )
+    AnnaChatBubble(
+        position = position,
+        onClick = onClick,
+    ) {
+        Text(
+            text = text,
+            color = colorResource(id = R.color.tigerds_chat_bubble_anna_text_color),
+        )
+    }
+}
+
+@Composable
+fun AnnaChatBubble(
+    modifier: Modifier = Modifier,
+    position: MessagePosition = MessagePosition.End,
+    onClick: () -> Unit = {},
+    content: @Composable BoxScope.() -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .clip(annaChatBubbleShape(position))
+            .background(colorResource(id = R.color.tigerds_chat_bubble_anna_background_color))
             .clickable { onClick() }
-            .padding(
-                start = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_start_spacing_inset_left),
-                end = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_start_spacing_inset_right),
-                top = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_start_spacing_inset_top),
-                bottom = dimensionResource(id = R.dimen.tigerds_bubble_chat_anna_start_spacing_inset_bottom),
-            ),
-        text = text,
-        color = colorResource(id = R.color.tigerds_bubble_chat_anna_text_color),
+            .padding(annaChatBubblePadding(position)),
+        content = content,
     )
 }
+
+@Composable
+private fun annaChatBubbleShape(position: MessagePosition) = when (position) {
+    MessagePosition.Start -> annaChatBubbleStartShape()
+    MessagePosition.Middle -> annaChatBubbleMiddleShape()
+    MessagePosition.End -> annaChatBubbleEndShape()
+}
+
+@Composable
+private fun annaChatBubbleStartShape() = RoundedCornerShape(
+    topStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_start_borderRadius_top_left),
+    topEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_start_borderRadius_top_right),
+    bottomStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_start_borderRadius_bottom_left),
+    bottomEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_start_borderRadius_bottom_right),
+)
+
+@Composable
+private fun annaChatBubbleMiddleShape() = RoundedCornerShape(
+    topStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_mid_borderRadius_top_left),
+    topEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_mid_borderRadius_top_right),
+    bottomStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_mid_borderRadius_bottom_left),
+    bottomEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_mid_borderRadius_bottom_right),
+)
+
+@Composable
+private fun annaChatBubbleEndShape() = RoundedCornerShape(
+    topStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_end_borderRadius_top_left),
+    topEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_end_borderRadius_top_right),
+    bottomStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_end_borderRadius_bottom_left),
+    bottomEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_end_borderRadius_bottom_right),
+)
+
+@Composable
+private fun annaChatBubblePadding(position: MessagePosition) = when (position) {
+    MessagePosition.Start -> annaChatBubbleStartPadding()
+    MessagePosition.Middle -> annaChatBubbleMiddlePadding()
+    MessagePosition.End -> annaChatBubbleEndPadding()
+}
+
+@Composable
+private fun annaChatBubbleStartPadding() = PaddingValues(
+    start = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_start_spacing_inset_left),
+    end = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_start_spacing_inset_right),
+    top = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_start_spacing_inset_top),
+    bottom = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_start_spacing_inset_bottom),
+)
+
+@Composable
+private fun annaChatBubbleMiddlePadding() = PaddingValues(
+    start = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_mid_spacing_inset_left),
+    end = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_mid_spacing_inset_right),
+    top = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_mid_spacing_inset_top),
+    bottom = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_mid_spacing_inset_bottom),
+)
+
+@Composable
+private fun annaChatBubbleEndPadding() = PaddingValues(
+    start = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_end_spacing_inset_left),
+    end = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_end_spacing_inset_right),
+    top = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_end_spacing_inset_top),
+    bottom = dimensionResource(id = R.dimen.tigerds_chat_bubble_anna_end_spacing_inset_bottom),
+)
 
 @Composable
 @Preview
@@ -92,12 +146,14 @@ class AnnaTextMessageView @JvmOverloads constructor(
 
     var text by mutableStateOf("")
     var position by mutableStateOf(MessagePosition.End)
+    var onClick: () -> Unit by mutableStateOf({})
 
     @Composable
     override fun Content() {
         AnnaTextMessage(
             text = text,
             position = position,
+            onClick = onClick,
         )
     }
 }
--- android/tiger-ui-kit/src/main/java/anna/money/tiger_ui_kit/text_message/UserTextMessage.kt
+++ android/tiger-ui-kit/src/main/java/anna/money/tiger_ui_kit/text_message/UserTextMessage.kt
@@ -5,7 +5,10 @@ import android.util.AttributeSet
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
@@ -29,44 +32,95 @@ fun UserTextMessage(
     position: MessagePosition,
     onClick: () -> Unit = {},
 ) {
-    Text(
-        modifier = Modifier
-            .clip(
-                shape = when (position) {
-                    MessagePosition.Start -> RoundedCornerShape(
-                        topStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_start_borderRadius_top_left),
-                        topEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_start_borderRadius_top_right),
-                        bottomStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_start_borderRadius_bottom_left),
-                        bottomEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_start_borderRadius_bottom_right),
-                    )
-                    MessagePosition.Middle -> RoundedCornerShape(
-                        topStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_mid_borderRadius_top_left),
-                        topEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_mid_borderRadius_top_right),
-                        bottomStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_mid_borderRadius_bottom_left),
-                        bottomEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_mid_borderRadius_bottom_right),
-                    )
-                    MessagePosition.End -> RoundedCornerShape(
-                        topStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_end_borderRadius_top_left),
-                        topEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_end_borderRadius_top_right),
-                        bottomStart = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_end_borderRadius_bottom_left),
-                        bottomEnd = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_end_borderRadius_bottom_right),
-                    )
-                },
-            )
-            .background(
-                color = colorResource(id = R.color.tigerds_bubble_chat_user_background_color),
-            )
+    UserChatBubble(
+        position = position,
+        onClick = onClick,
+    ) {
+        Text(
+            text = text,
+            color = colorResource(id = R.color.tigerds_chat_bubble_user_text_color),
+        )
+    }
+}
+
+@Composable
+fun UserChatBubble(
+    modifier: Modifier = Modifier,
+    position: MessagePosition = MessagePosition.End,
+    onClick: () -> Unit = {},
+    content: @Composable BoxScope.() -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .clip(userChatBubbleShape(position))
+            .background(colorResource(id = R.color.tigerds_chat_bubble_user_background_color))
             .clickable { onClick() }
-            .padding(
-                start = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_start_spacing_inset_left),
-                end = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_start_spacing_inset_right),
-                top = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_start_spacing_inset_top),
-                bottom = dimensionResource(id = R.dimen.tigerds_bubble_chat_user_start_spacing_inset_bottom),
-            ),
-        text = text,
-        color = colorResource(id = R.color.tigerds_bubble_chat_user_text_color),
+            .padding(userChatBubblePadding(position)),
+        content = content,
     )
 }
+
+@Composable
+private fun userChatBubbleShape(position: MessagePosition) = when (position) {
+    MessagePosition.Start -> userChatBubbleStartShape()
+    MessagePosition.Middle -> userChatBubbleMiddleShape()
+    MessagePosition.End -> userChatBubbleEndShape()
+}
+
+@Composable
+private fun userChatBubbleStartShape() = RoundedCornerShape(
+    topStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_start_borderRadius_top_left),
+    topEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_start_borderRadius_top_right),
+    bottomStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_start_borderRadius_bottom_left),
+    bottomEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_start_borderRadius_bottom_right),
+)
+
+@Composable
+private fun userChatBubbleMiddleShape() = RoundedCornerShape(
+    topStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_mid_borderRadius_top_left),
+    topEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_mid_borderRadius_top_right),
+    bottomStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_mid_borderRadius_bottom_left),
+    bottomEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_mid_borderRadius_bottom_right),
+)
+
+@Composable
+private fun userChatBubbleEndShape() = RoundedCornerShape(
+    topStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_end_borderRadius_top_left),
+    topEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_end_borderRadius_top_right),
+    bottomStart = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_end_borderRadius_bottom_left),
+    bottomEnd = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_end_borderRadius_bottom_right),
+)
+
+@Composable
+private fun userChatBubblePadding(position: MessagePosition) = when (position) {
+    MessagePosition.Start -> userChatBubbleStartPadding()
+    MessagePosition.Middle -> userChatBubbleMiddlePadding()
+    MessagePosition.End -> userChatBubbleEndPadding()
+}
+
+@Composable
+private fun userChatBubbleStartPadding() = PaddingValues(
+    start = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_start_spacing_inset_left),
+    end = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_start_spacing_inset_right),
+    top = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_start_spacing_inset_top),
+    bottom = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_start_spacing_inset_bottom),
+)
+
+@Composable
+private fun userChatBubbleMiddlePadding() = PaddingValues(
+    start = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_mid_spacing_inset_left),
+    end = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_mid_spacing_inset_right),
+    top = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_mid_spacing_inset_top),
+    bottom = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_mid_spacing_inset_bottom),
+)
+
+@Composable
+private fun userChatBubbleEndPadding() = PaddingValues(
+    start = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_end_spacing_inset_left),
+    end = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_end_spacing_inset_right),
+    top = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_end_spacing_inset_top),
+    bottom = dimensionResource(id = R.dimen.tigerds_chat_bubble_user_end_spacing_inset_bottom),
+)
 
 @Composable
 @Preview
@@ -92,12 +146,14 @@ class UserTextMessageView @JvmOverloads constructor(
 
     var text by mutableStateOf("")
     var position by mutableStateOf(MessagePosition.End)
+    var onClick: () -> Unit by mutableStateOf({})
 
     @Composable
     override fun Content() {
         UserTextMessage(
             text = text,
             position = position,
+            onClick = onClick,
         )
     }
 }
